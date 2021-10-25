e7920611502e0f9139702db2d4028569d0c358ee
Skronak <narvak.skronak@gmail.com>
2021-10-25 16:47:27 +0200

    Update lobby.js
    adaptation lobby + testing

--- lobby.js
+++ lobby.js
@@ -1,62 +1,62 @@
 const LOBBY = "lobby";
-var Player = require('./player');
+let Player = require('./player');
 
 class Lobby {
 
     constructor(socketIO) {
         this.io = socketIO;
         this.rooms = ["Hanin", "Naehara", "Kzee"];
-        this.players = [];
+        this.clients = [];
 
         this.listen();
     }
 
     listen() {
-        this.io.sockets.on('connection', this.handleConnection);
-    }
-    
-    handleConnection = (socket) => {
-        socket.emit('connect');
-        socket.on('register', (data) => this.registerUser(socket, data));
-        socket.on('addRoom', (room) => this.addRoom(socket, room));
-        socket.on('joinRoom', (room) => this.joinRoom(socket, room));
+        this.io.sockets.on('connection', (socket) => {
+            socket.on('register', (data) => this.registerUser(socket, data));
+            socket.on('addRoom', (room) => this.addRoom(socket, room));
+            socket.on('joinRoom', (room) => this.joinRoom(socket, room));
+        });
     }
 
-    registerUser = (socket, playerName) => {
+    registerUser (socket, playerName) {
+        console.log('registerUser' + socket.id);
+        socket.join(LOBBY);
+
         let player = new Player();
         player.username = playerName;
         player.id = socket.id;
-        this.players[player.id] = player;
-        socket.join(LOBBY);
-        socket.emit(rooms);
+        player.roomId = LOBBY;
+        this.clients[player.id] = player;
 
         this.informUsers(socket, player);
         this.getRooms(socket);
     }
 
-    informUsers = (socket, currentPlayer) => {
-        console.log(this.players.filter(player => player.id != currentPlayer.id ));
-        this.players.filter(player => player.id != currentPlayer.id )
+    informUsers (socket, currentPlayer) {
+        const other = this.io.sockets.adapter.rooms.get(currentPlayer.roomId);
+        // Renvoie la liste des autres player a celui connecte
+        //console.log(this.clients.filter(player => player.id != currentPlayer.id ));
+        this.clients.filter(player => player.id != currentPlayer.id )
                .map(player => {
                     socket.to(LOBBY).emit('playerConnection', player);
                 });
+
         socket.broadcast.to(LOBBY).emit('playerConnection', currentPlayer);
-        // lister membre du lobby
     }
 
-    joinRoom = (socket, room) => {
+    joinRoom (socket, room) {
         socket.emit('joinRoom', room); // client must ask game serv room
         socket.leave(LOBBY);
-        this.players = this.players.filter(player => player.id != socket.id);
-
+        this.clients = this.clients.filter(player => player.id != socket.id);
     }
     
-    addRoom = (socket, room) => {
+    addRoom (socket, room) {
             rooms.push(room);
             socket.emit('updaterooms', rooms, socket.room);
     }
 
-    getRooms = (socket) => {
+    getRooms (socket) {
         socket.emit('rooms', { listRoom : this.rooms });
     }
 }
