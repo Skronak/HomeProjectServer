b995f58cf0ea6a52c8f9eeb6cd988e158786a2bb
Skronak <narvak.skronak@gmail.com>
2021-10-28 17:21:28 +0200

    Update lobby.js
    room management

--- lobby.js
+++ lobby.js
@@ -1,21 +1,33 @@
+require('log-timestamp');
+
 const LOBBY = "lobby";
 let Player = require('./player');
+let Room = require('./room');
+
+const ROOM_ID = ["Hanin", "Naehara", "Kzee"];
+const ROOM_STATUTS = {
+    PENDING: 'pending',
+    READY: 'ready',
+    BUSY: 'busy'
+};
 
 class Lobby {
 
     constructor(socketIO) {
         this.io = socketIO;
-        this.rooms = ["Hanin", "Naehara", "Kzee"];
+        this.rooms = {};
         this.clients = {};
 
         this.listen();
     }
 
     listen() {
+        ROOM_ID.forEach(roomId => this.rooms[roomId] =new Room(roomId));
         this.io.sockets.on('connection', (socket) => {
             socket.on('register', (data) => this.registerUser(socket, data));
             socket.on('addRoom', (room) => this.addRoom(socket, room));
             socket.on('joinRoom', (room) => this.joinRoom(socket, room));
+            socket.on('disconnect', () => this.disconnectUser(socket));
         });
     }
 
@@ -30,25 +42,49 @@ class Lobby {
         this.clients[player.id] = player;
 
         this.informUsers(socket, player);
-        this.getRooms(socket);
+        this.listRooms(socket);
     }
 
     informUsers (socket, currentPlayer) {
         //const other = this.io.sockets.adapter.rooms.get(currentPlayer.roomId);
-        // Renvoie la liste des autres player a celui connecte
+        // Renvoie la liste des autres cvlient a celui connecte
         Object.keys(this.clients).filter(clientId => this.clients[clientId].id !== currentPlayer.id)
             .forEach(client=> {
-                    console.log('client '+client);
-                    socket.to(LOBBY).emit('playerConnection', this.clients[client].id);
-                }
-            );
-        socket.broadcast.to(LOBBY).emit('playerConnection', currentPlayer);
+                socket.to(LOBBY).emit('playerConnection', this.clients[client].id);
+            });
+        setTimeout(()=> {
+            socket.broadcast.to(LOBBY).emit('playerConnection', currentPlayer); // for testing, too fast otherwise
+        },1000);
     }
 
-    joinRoom (socket, room) {
-        socket.emit('joinRoom', room); // client must ask game serv room
-        socket.leave(LOBBY);
-        this.clients = this.clients.filter(player => player.id != socket.id);
+    listRooms(socket) {
+        socket.emit('roomList', this.rooms);
+    }
+
+    getRoomDetails(socket, roomId) {
+        //https://stackoverflow.com/questions/19044660/socket-io-get-rooms-which-socket-is-currently-in
+        //https://newbedev.com/socket-io-rooms-get-list-of-clients-in-specific-room
+        //const clients = io.sockets.adapter.rooms.get('Room Name');
+        //const clientSocket = io.sockets.sockets.get(clientId);
+
+        console.log('detailsRoom');
+
+        const clients = this.io.sockets.adapter.rooms[roomId].sockets;
+        this.rooms.length = room ? Object.keys(clients).length : 0;
+        socket.emit('roomDetail', this.room);
+    }
+
+    joinRoom (socket, roomId) {
+        console.log(`joinRoom: ${socket.id} => ${roomId}`);
+
+        if(this.rooms.hasOwnProperty(roomId)) {
+            socket.join(roomId);//attention room sont automatiquement delete si personne dedans
+            socket.leave(LOBBY);
+
+            const clients = this.io.sockets.adapter.rooms[roomId].sockets;
+            this.rooms[roomId] = Object.keys(clients).length;
+            this.getRoomDetails(socket, this.roomss[roomId]);
+        }
     }
     
     addRoom (socket, room) {
@@ -56,9 +92,12 @@ class Lobby {
             socket.emit('updaterooms', rooms, socket.room);
     }
 
-    getRooms (socket) {
-        socket.emit('rooms', { listRoom : this.rooms });
+    disconnectUser(socket) {
+        console.log(`disconnectUser ${socket.id}`);
+        delete this.clients[socket.id];
+        socket.broadcast.emit('playerDisconnection', {id: socket.id});
     }
+
 }
 
 module.exports = Lobby;
