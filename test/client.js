be4a0843acaa88a1141c05c126d7ecd3c1061815
Skronak <narvak.skronak@gmail.com>
2021-11-16 16:48:57 +0100

    Update client.js
    adding 3rd client for room testing

--- test/client.js
+++ test/client.js
@@ -1,4 +1,4 @@
-const io = require("socket.io-client");
+const { io } = require("socket.io-client");
 require('log-timestamp');
 
 let serverPort = 3000;
@@ -8,12 +8,13 @@ if (p_port) {
 }
 const socket1 = io('http://localhost:' + serverPort);
 const socket2 = io('http://localhost:' + serverPort);
+
 console.log('Started on port '+ serverPort);
 
 let cardsSocket1, cardsSocket2;
 
-socket1.on('connect', () => {
-    console.log('P1: connected');
+socket1.on('connect', (data) => {
+    console.log(`P1: connected as ${socket1.id}`);
     socket1.emit('register','Player1');
 
     socket1.on('playerConnection', (player) => {
@@ -37,41 +38,65 @@ socket1.on('connect', () => {
     });
 
     socket1.on('roomList', (data) => {
-        console.log(`P1: RoomList ${JSON.stringify(Object.keys(data))}`);
+//        console.log(`P1: RoomList ${JSON.stringify(data)}`);
         socket1.emit('joinRoom', data[Object.keys(data)[0]].id);
     });
 
-    socket1.on('roomDetail', (data)=> {
-        console.log(data);
-        console.log(JSON.stringify(data));
+    socket1.on('chat', (data)=> {
+        console.log('chat');
+    })
+    socket1.on('roomInfos', (data)=> {
+//        console.log(JSON.stringify(data));
     })
 });
 
 socket2.on('connect', () => {
-    console.log('P2: connected');
-    socket2.emit('register','Player2');
+    console.log(`P2: connected as ${socket2.id}`);
 
-    socket2.on('playerConnection', (player) => {
-        console.log(`P2: playerConnection: ${player.id}`)
-    });
+    setTimeout(()=> {
+        socket2.emit('register','Player2');
 
-    socket2.on('connectionEstabilished',({id})=> {
-        console.log('P2: connectionEstabilished'+id);
-    });
+        socket2.on('playerConnection', (player) => {
+            console.log(`P2: playerConnection: ${player.id}`)
+        });
+        socket2.on('chat', (data)=> {
+            console.log('chat');
+        })
 
-    socket2.on('sendCard', (cards) => {
-        console.log('P2: sendCard');
-        cardsSocket2 = cards.hand;
-    });
+        socket2.on('connectionEstabilished',({id})=> {
+            console.log('P2: connectionEstabilished'+id);
+        });
 
-    socket2.on('token', (data) => {
-        if (data.token === socket2.id) {
-            console.log(`P2: emit reveal card ${cardsSocket1[0].id}`);
-            socket2.emit('revealCard', cardsSocket1[0].id);
-        }
-    })
+        socket2.on('sendCard', (cards) => {
+            console.log('P2: sendCard');
+            cardsSocket2 = cards.hand;
+        });
 
-    socket2.on('roomList', (data) => {
-        console.log(`P2: RoomList ${JSON.stringify(data)}`);
-    })
+        socket2.on('token', (data) => {
+            if (data.token === socket2.id) {
+                console.log(`P2: emit reveal card ${cardsSocket1[0].id}`);
+                socket2.emit('revealCard', cardsSocket1[0].id);
+            }
+        })
+
+        socket2.on('roomList', (data) => {
+//            console.log(`P2: RoomList ${JSON.stringify(data)}`);
+        })
+    },3000);
 })
+
+setTimeout(()=> {
+    const socket3 = io('http://localhost:' + serverPort);
+
+    socket3.on('connect', (data) => {
+        setTimeout(()=> {
+            console.log(`P3: connected as ${socket3.id}`);
+            socket3.emit('register', 'Player3');
+            socket3.emit('chat', 'chat');
+        }, 5000);
+
+        socket3.on('playerConnection', (player) => {
+            console.log(`P3: playerConnection: ${player.id}`)
+        });
+    })
+}, 5000);
